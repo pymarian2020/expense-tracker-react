c6f5ef097185521e2030a3bce3046431fafe5d2a
Marian <mariukus@gmail.com>
2021-12-14 09:56:30 +0100

    Updates

---
+++ precache-manifest.053f01efdfe39375f009a0ca5bedcc82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf7a3a36d97a03dd3d36faf77c1c274",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "afa856b18f1e2df3e2fd",
+    "url": "/expense-tracker-react/static/css/main.529b1d88.chunk.css"
+  },
+  {
+    "revision": "98719b8117d734ae377a",
+    "url": "/expense-tracker-react/static/js/2.f7071f73.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/expense-tracker-react/static/js/2.f7071f73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa856b18f1e2df3e2fd",
+    "url": "/expense-tracker-react/static/js/main.2125be8f.chunk.js"
+  },
+  {
+    "revision": "67a4669715835298d50d",
+    "url": "/expense-tracker-react/static/js/runtime-main.3587cd2a.js"
+  }
+]);
